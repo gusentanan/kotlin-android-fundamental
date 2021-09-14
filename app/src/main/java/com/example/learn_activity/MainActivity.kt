5eef9a743a9438cc11877321440f24b879a046ed
Bagus Merta <merta.bagus24@gmail.com>
2021-09-14 12:50:48 +0800

    build: implement intent

--- app/src/main/java/com/example/learn_activity/MainActivity.kt
+++ app/src/main/java/com/example/learn_activity/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.learn_activity
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -14,6 +15,8 @@ import android.widget.TextView
 // main class with onClickListener view for button component
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var btnMoveActivity: Button
+    private  lateinit var btnMoveActivityData: Button
+    private lateinit var btnMoveImplicit: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +24,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         btnMoveActivity = findViewById(R.id.btn_explicit)
         btnMoveActivity.setOnClickListener(this)
+
+        btnMoveActivityData = findViewById(R.id.btn_explicit_data)
+        btnMoveActivityData.setOnClickListener(this)
+
+        btnMoveImplicit = findViewById(R.id.btn_implicit)
+        btnMoveImplicit.setOnClickListener(this)
     }
 
     override fun onClick(p: View?) {
@@ -28,6 +37,17 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             R.id.btn_explicit -> {
                 val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
                 startActivity(moveIntent)
+            }
+            R.id.btn_explicit_data -> {
+                val moveWithData = Intent(this@MainActivity, MoveActivityData::class.java)
+                moveWithData.putExtra(MoveActivityData.EXTRA_NAME, "Hello semeton!")
+                moveWithData.putExtra(MoveActivityData.EXTRA_AGE, 20)
+                startActivity(moveWithData)
+            }
+            R.id.btn_implicit -> {
+                val url = "http://www.facebook.com"
+                val moveImplicit = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                startActivity(moveImplicit)
             }
         }
     }
