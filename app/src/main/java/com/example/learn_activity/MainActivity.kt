5523f45bef5a599869872ca0dd1e709b3637e0fe
Bagus Merta <merta.bagus24@gmail.com>
2021-10-18 22:41:27 +0800

    add tablayout example

--- app/src/main/java/com/example/learn_activity/MainActivity.kt
+++ app/src/main/java/com/example/learn_activity/MainActivity.kt
@@ -1,18 +1,34 @@
 package com.example.learn_activity
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.learn_activity.databinding.ActivityMainBinding
+import android.os.Bundle
+import androidx.annotation.StringRes
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    companion object {
+        @StringRes
+        private val TAB_TITLES = intArrayOf(
+            R.string.tab_text_1,
+            R.string.tab_text_2,
+            R.string.tab_text_3
+        )
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.tvWelcome.text = "Hello Bagus!"
+        setContentView(R.layout.activity_main)
+
+        val sectionsPagerAdapter = SectionsPagerAdapter(this)
+        val viewPager: ViewPager2 = findViewById(R.id.view_pager)
+        viewPager.adapter = sectionsPagerAdapter
+        val tabs: TabLayout = findViewById(R.id.tabs)
+        TabLayoutMediator(tabs, viewPager) { tab, position ->
+            tab.text = resources.getString(TAB_TITLES[position])
+        }.attach()
+        supportActionBar?.elevation = 0f
     }
 }
-
