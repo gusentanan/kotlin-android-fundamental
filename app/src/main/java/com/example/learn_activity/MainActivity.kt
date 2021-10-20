c79b9b7ffc922ad8b27cd6abfcbcc05b7460b9b8
Bagus Merta <merta.bagus24@gmail.com>
2021-10-20 21:10:11 +0800

    add file storage implemetation

--- app/src/main/java/com/example/learn_activity/MainActivity.kt
+++ app/src/main/java/com/example/learn_activity/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.learn_activity
 import android.os.Bundle
 import android.text.TextUtils
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -13,71 +15,60 @@ import com.example.learn_activity.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    private lateinit var activityMainBinding: ActivityMainBinding
-    private lateinit var mainViewModel: MainViewModel
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-                activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
-                setContentView(activityMainBinding.root)
-                mainViewModel = MainViewModel(CuboidModel())
-                activityMainBinding.btnSave.setOnClickListener(this)
-                activityMainBinding.btnCalculateSurfaceArea.setOnClickListener(this)
-                activityMainBinding.btnCalculateCircumference.setOnClickListener(this)
-                activityMainBinding.btnCalculateVolume.setOnClickListener(this)
+        binding.buttonNew.setOnClickListener(this)
+        binding.buttonOpen.setOnClickListener(this)
+        binding.buttonSave.setOnClickListener(this)
     }
 
-    override fun onClick(v: View) {
-        val length = activityMainBinding.edtLength.text.toString().trim()
-        val width = activityMainBinding.edtWidth.text.toString().trim()
-        val height = activityMainBinding.edtHeight.text.toString().trim()
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.button_new -> newFile()
+                R.id.button_open -> showList()
+            R.id.button_save -> saveFile()
+        }
+    }
+
+    private fun newFile() {
+        binding.editTitle.setText("")
+        binding.editFile.setText("")
+        Toast.makeText(this, "Clearing file", Toast.LENGTH_SHORT).show()
+    }
+    private fun showList() {
+        val items = fileList()
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Pilih file yang diinginkan")
+        builder.setItems(items) { dialog, item -> loadData(items[item].toString()) }
+        val alert = builder.create()
+        alert.show()
+    }
+    private fun loadData(title: String) {
+        val fileModel = FileHelper.readFromFile(this, title)
+        binding.editTitle.setText(fileModel.filename)
+        binding.editFile.setText(fileModel.data)
+        Toast.makeText(this, "Loading " + fileModel.filename + " data", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun saveFile() {
         when {
-            TextUtils.isEmpty(length) -> {
-                activityMainBinding.edtLength.error = "Field ini tidak boleh kosong"
-            }
-            TextUtils.isEmpty(width) -> {
-                activityMainBinding.edtWidth.error = "Field ini tidak boleh kosong"
-            }
-            TextUtils.isEmpty(height) -> {
-                activityMainBinding.edtHeight.error = "Field ini tidak boleh kosong"
-            }
+            binding.editTitle.text.toString().isEmpty() -> Toast.makeText(this, "Title harus diisi terlebih dahulu", Toast.LENGTH_SHORT).show()
+            binding.editFile.text.toString().isEmpty() -> Toast.makeText(this, "Kontent harus diisi terlebih dahulu", Toast.LENGTH_SHORT).show()
             else -> {
-                val valueLength = length.toDouble()
-                val valueWidth = width.toDouble()
-                val valueHeight = height.toDouble()
-                when (v.id) {
-                    R.id.btn_save -> {
-                        mainViewModel.save(valueLength, valueWidth, valueHeight)
-                        visible()
-                    }
-                    R.id.btn_calculate_circumference -> {
-                        activityMainBinding.tvResult.text = mainViewModel.getCircumference().toString()
-                        gone()
-                    }
-                    R.id.btn_calculate_surface_area -> {
-                        activityMainBinding.tvResult.text = mainViewModel.getSurfaceArea().toString()
-                        gone()
-                    }
-                    R.id.btn_calculate_volume -> {
-                        activityMainBinding.tvResult.text = mainViewModel.getVolume().toString()
-                        gone()
-                    }
-                }
+                val title = binding.editTitle.text.toString()
+                val text = binding.editFile.text.toString()
+                val fileModel = FileModel()
+                fileModel.filename = title
+                fileModel.data = text
+                FileHelper.writeToFile(fileModel, this)
+                Toast.makeText(this, "Saving " + fileModel.filename + " file", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
-    private fun visible() {
-        activityMainBinding.btnCalculateVolume.visibility = View.VISIBLE
-        activityMainBinding.btnCalculateCircumference.visibility = View.VISIBLE
-        activityMainBinding.btnCalculateSurfaceArea.visibility = View.VISIBLE
-        activityMainBinding.btnSave.visibility = View.GONE
-    }
-    private fun gone() {
-        activityMainBinding.btnCalculateVolume.visibility = View.GONE
-        activityMainBinding.btnCalculateCircumference.visibility = View.GONE
-        activityMainBinding.btnCalculateSurfaceArea.visibility = View.GONE
-        activityMainBinding.btnSave.visibility = View.VISIBLE
-    }
 }
