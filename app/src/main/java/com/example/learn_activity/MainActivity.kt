08c05da200f0bb6d7624d59672bf108eccf859b7
Bagus Merta <merta.bagus24@gmail.com>
2021-10-20 12:57:40 +0800

    add unit testing example

--- app/src/main/java/com/example/learn_activity/MainActivity.kt
+++ app/src/main/java/com/example/learn_activity/MainActivity.kt
@@ -1,29 +1,82 @@
 package com.example.learn_activity
 
 import android.os.Bundle
+import android.text.TextUtils
+import android.view.View
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.learn_activity.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+    private lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        val navController = findNavController(R.id.nav_host_fragment)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications, R.id.navigation_profile
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+                activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+                setContentView(activityMainBinding.root)
+                mainViewModel = MainViewModel(CuboidModel())
+                activityMainBinding.btnSave.setOnClickListener(this)
+                activityMainBinding.btnCalculateSurfaceArea.setOnClickListener(this)
+                activityMainBinding.btnCalculateCircumference.setOnClickListener(this)
+                activityMainBinding.btnCalculateVolume.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View) {
+        val length = activityMainBinding.edtLength.text.toString().trim()
+        val width = activityMainBinding.edtWidth.text.toString().trim()
+        val height = activityMainBinding.edtHeight.text.toString().trim()
+        when {
+            TextUtils.isEmpty(length) -> {
+                activityMainBinding.edtLength.error = "Field ini tidak boleh kosong"
+            }
+            TextUtils.isEmpty(width) -> {
+                activityMainBinding.edtWidth.error = "Field ini tidak boleh kosong"
+            }
+            TextUtils.isEmpty(height) -> {
+                activityMainBinding.edtHeight.error = "Field ini tidak boleh kosong"
+            }
+            else -> {
+                val valueLength = length.toDouble()
+                val valueWidth = width.toDouble()
+                val valueHeight = height.toDouble()
+                when (v.id) {
+                    R.id.btn_save -> {
+                        mainViewModel.save(valueLength, valueWidth, valueHeight)
+                        visible()
+                    }
+                    R.id.btn_calculate_circumference -> {
+                        activityMainBinding.tvResult.text = mainViewModel.getCircumference().toString()
+                        gone()
+                    }
+                    R.id.btn_calculate_surface_area -> {
+                        activityMainBinding.tvResult.text = mainViewModel.getSurfaceArea().toString()
+                        gone()
+                    }
+                    R.id.btn_calculate_volume -> {
+                        activityMainBinding.tvResult.text = mainViewModel.getVolume().toString()
+                        gone()
+                    }
+                }
+            }
+        }
+    }
+    private fun visible() {
+        activityMainBinding.btnCalculateVolume.visibility = View.VISIBLE
+        activityMainBinding.btnCalculateCircumference.visibility = View.VISIBLE
+        activityMainBinding.btnCalculateSurfaceArea.visibility = View.VISIBLE
+        activityMainBinding.btnSave.visibility = View.GONE
+    }
+    private fun gone() {
+        activityMainBinding.btnCalculateVolume.visibility = View.GONE
+        activityMainBinding.btnCalculateCircumference.visibility = View.GONE
+        activityMainBinding.btnCalculateSurfaceArea.visibility = View.GONE
+        activityMainBinding.btnSave.visibility = View.VISIBLE
     }
 }
