3c9be82eccb8ec39d0a72973d36b435652732b04
Bagus Merta <merta.bagus24@gmail.com>
2021-10-21 12:01:11 +0800

    add data store implementation

--- app/src/main/java/com/example/learn_activity/MainActivity.kt
+++ app/src/main/java/com/example/learn_activity/MainActivity.kt
@@ -1,99 +1,50 @@
 package com.example.learn_activity
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.CompoundButton
 import android.widget.Toast
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
+import androidx.lifecycle.ViewModelProvider
 import com.example.learn_activity.databinding.ActivityMainBinding
+import com.google.android.material.switchmaterial.SwitchMaterial
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
-    private lateinit var mUserPreference: UserPreference
-    private lateinit var binding: ActivityMainBinding
-
-    private var isPreferenceEmpty = false
-    private lateinit var userModel: UserModel
-
-    private val resultLauncher = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult()
-    ) { result: ActivityResult ->
-        if (result.data != null && result.resultCode == FormUserPreferenceActivity.RESULT_CODE) {
-            userModel = result.data?.getParcelableExtra<UserModel>(FormUserPreferenceActivity.EXTRA_RESULT) as UserModel
-            populateView(userModel)
-            checkForm(userModel)
-        }
-    }
+    private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "settings")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        setContentView(R.layout.activity_main)
 
-        supportActionBar?.title = "My User Preference"
+        val switchTheme = findViewById<SwitchMaterial>(R.id.switch_theme)
 
-        mUserPreference = UserPreference(this)
-
-        showExistingPreference()
-
-        binding.btnSave.setOnClickListener(this)
-
-    }
-
-    private fun showExistingPreference() {
-        userModel = mUserPreference.getUser()
-        populateView(userModel)
-        checkForm(userModel)
-    }
-
-    private fun populateView(userModel: UserModel) {
-        binding.tvName.text =
-            if (userModel.name.toString().isEmpty()) "Tidak Ada" else userModel.name
-        binding.tvAge.text =
-            if (userModel.age.toString().isEmpty()) "Tidak Ada" else userModel.age.toString()
-        binding.tvIsLoveMu.text = if (userModel.isLove) "Ya" else "Tidak"
-        binding.tvEmail.text =
-            if (userModel.email.toString().isEmpty()) "Tidak Ada" else userModel.email
-        binding.tvPhone.text =
-            if (userModel.phoneNumber.toString().isEmpty()) "Tidak Ada" else userModel.phoneNumber
-    }
-
-    private fun checkForm(userModel: UserModel) {
-        when {
-            userModel.name.toString().isNotEmpty() -> {
-                binding.btnSave.text = getString(R.string.change)
-                isPreferenceEmpty = false
-            }
-            else -> {
-                binding.btnSave.text = getString(R.string.save)
-                isPreferenceEmpty = true
+        val pref = SettingPreferences.getInstance(dataStore)
+        val mainViewModel = ViewModelProvider(this, ViewModelFactory(pref)).get(
+            MainViewModel::class.java
+        )
+        mainViewModel.getThemeSettings().observe(this
+        ) { isDarkModeActive: Boolean ->
+            if (isDarkModeActive) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                switchTheme.isChecked = true
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                switchTheme.isChecked = false
             }
         }
-    }
 
-    override fun onClick(view: View) {
-        if (view.id == R.id.btn_save) {
-            val intent = Intent(this@MainActivity, FormUserPreferenceActivity::class.java)
-            when {
-                isPreferenceEmpty -> {
-                    intent.putExtra(
-                        FormUserPreferenceActivity.EXTRA_TYPE_FORM,
-                        FormUserPreferenceActivity.TYPE_ADD
-                    )
-                    intent.putExtra("USER", userModel)
-                }
-                else -> {
-                    intent.putExtra(
-                        FormUserPreferenceActivity.EXTRA_TYPE_FORM,
-                        FormUserPreferenceActivity.TYPE_EDIT
-                    )
-                    intent.putExtra("USER", userModel)
-                }
-            }
-            resultLauncher.launch(intent)
+        switchTheme.setOnCheckedChangeListener { _: CompoundButton?, isChecked: Boolean ->
+            mainViewModel.saveThemeSetting(isChecked)
         }
     }
 }
